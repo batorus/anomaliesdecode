0136d887ce2cb030162872355b7858891bfbd447
batorus <batoru@yahoo.com>
2018-05-16 18:26:17 +0300

    wip generate crud for typesociete

---
+++ src/AnomaliesBundle/Form/TypesocieteType.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace AnomaliesBundle\Form;
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolverInterface;
+
+class TypesocieteType extends AbstractType
+{
+    /**
+     * @param FormBuilderInterface $builder
+     * @param array $options
+     */
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('typesociete')
+            ->add('enabled')
+            ->add('controles')
+        ;
+    }
+    
+    /**
+     * @param OptionsResolverInterface $resolver
+     */
+    public function setDefaultOptions(OptionsResolverInterface $resolver)
+    {
+        $resolver->setDefaults(array(
+            'data_class' => 'AnomaliesBundle\Entity\Typesociete'
+        ));
+    }
+
+    /**
+     * @return string
+     */
+    public function getName()
+    {
+        return 'anomaliesbundle_typesociete';
+    }
+}
