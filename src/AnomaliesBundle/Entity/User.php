b7b1a7941a0682b3e3202030d9782b8399e3f693
batorus <batoru@yahoo.com>
2018-05-17 14:59:02 +0300

    wip

---
+++ src/AnomaliesBundle/Entity/User.php
@@ -0,0 +1,89 @@
+<?php
+
+namespace AnomaliesBundle\Entity;
+
+
+use FOS\UserBundle\Model\User as BaseUser;
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * User
+ *
+ * @ORM\Table(name="ar_user", uniqueConstraints={@ORM\UniqueConstraint(name="UNIQ_8D93D64992FC23A8", columns={"username_canonical"}), @ORM\UniqueConstraint(name="UNIQ_8D93D649A0D96FBF", columns={"email_canonical"})})
+ * @ORM\Entity
+ */
+class User extends BaseUser
+{
+    
+       
+    /**
+     * @var integer
+     *
+     * @ORM\Column(name="id", type="integer")
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    protected $id;
+
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="matricule", type="string", length=6, options={"collation":"utf8_general_ci"}, nullable=true)
+     */
+    private $matricule;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="photo", type="string", length=255, options={"collation":"utf8_general_ci"}, nullable=true)
+     */
+    private $photo;
+
+
+    /**
+     * Set matricule
+     *
+     * @param string $matricule
+     * @return ArUser
+     */
+    public function setMatricule($matricule)
+    {
+        $this->matricule = $matricule;
+
+        return $this;
+    }
+
+    /**
+     * Get matricule
+     *
+     * @return string 
+     */
+    public function getMatricule()
+    {
+        return $this->matricule;
+    }
+
+    /**
+     * Set photo
+     *
+     * @param string $photo
+     * @return ArUser
+     */
+    public function setPhoto($photo)
+    {
+        $this->photo = $photo;
+
+        return $this;
+    }
+
+    /**
+     * Get photo
+     *
+     * @return string 
+     */
+    public function getPhoto()
+    {
+        return $this->photo;
+    }
+}
