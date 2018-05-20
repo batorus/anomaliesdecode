1e8d37e3965d77e2c1011380a5a3564cf9d5ca2a
batorus <batoru@yahoo.com>
2018-05-20 23:50:01 +0300

    fara erori

--- src/AnomaliesBundle/Command/CreateUserCommand.php
+++ src/AnomaliesBundle/Command/CreateUserCommand.php
@@ -24,20 +24,7 @@ class CreateUserCommand extends ContainerAwareCommand
         $this->addArgument('username', InputArgument::REQUIRED, 'The username of the user.');
     }
 
-	
-	  protected function configure()
- //   {
-        $this
-         // the name of the command (the part after "app/console")
-        ->setName('app:create-user')
 
-        // the short description shown while running "php app/console list"
-        ->setDescription('Creates a new user.')
-        // the full command description shown when running the command with
-        // the "--help" option
-        ->setHelp('This command allows you to create a user...');
-        $this->addArgument('username', InputArgument::REQUIRED, 'The username of the user.');
-   // }
     protected function execute(InputInterface $input, OutputInterface $output)
     {
          // outputs multiple lines to the console (adding "\n" at the end of each line)
