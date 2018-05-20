d81e1badec96fe243afeabf6fb2ae49dc6a486e1
batorus <batoru@yahoo.com>
2018-05-20 23:37:22 +0300

    wip comment

--- src/AnomaliesBundle/Command/CreateUserCommand.php
+++ src/AnomaliesBundle/Command/CreateUserCommand.php
@@ -10,19 +10,19 @@ use Symfony\Bundle\FrameworkBundle\Command\ContainerAwareCommand;
 
 class CreateUserCommand extends ContainerAwareCommand
 {
-    protected function configure()
-    {
-        $this
-         // the name of the command (the part after "app/console")
-        ->setName('app:create-user')
-
-        // the short description shown while running "php app/console list"
-        ->setDescription('Creates a new user.')
-        // the full command description shown when running the command with
-        // the "--help" option
-        ->setHelp('This command allows you to create a user...');
-        $this->addArgument('username', InputArgument::REQUIRED, 'The username of the user.');
-    }
+//    protected function configure()
+//    {
+//        $this
+//         // the name of the command (the part after "app/console")
+//        ->setName('app:create-user')
+//
+//        // the short description shown while running "php app/console list"
+//        ->setDescription('Creates a new user.')
+//        // the full command description shown when running the command with
+//        // the "--help" option
+//        ->setHelp('This command allows you to create a user...');
+//        $this->addArgument('username', InputArgument::REQUIRED, 'The username of the user.');
+//    }
 
     protected function execute(InputInterface $input, OutputInterface $output)
     {
