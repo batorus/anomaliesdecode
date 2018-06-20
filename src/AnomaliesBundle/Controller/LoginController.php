e836ca5786821798aff4e719d52d0d46ead35a23
batorus <batoru@yahoo.com>
2018-06-20 16:10:09 +0300

    wip

--- src/AnomaliesBundle/Controller/LoginController.php
+++ src/AnomaliesBundle/Controller/LoginController.php
@@ -6,10 +6,23 @@ use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 
 class LoginController extends Controller
 {
-    public function loginAction()
-    {
-        return $this->render('AnomaliesBundle:Login:login.html.twig', array(
-                // ...
-            ));    }
+    
+    public function loginAction(Request $request){
+        $authenticationUtils = $this->get('security.authentication_utils');
+
+        // get the login error if there is one
+        $error = $authenticationUtils->getLastAuthenticationError();
+
+        // last username entered by the user
+        $lastUsername = $authenticationUtils->getLastUsername();
+
+
+        return $this->render('AnomaliesBundle:Login:login.html.twig', 
+                                array(
+                                     'last_username' => $lastUsername,
+                                     'error'         => $error,
+                                    )
+                            );        
+    }
 
 }
